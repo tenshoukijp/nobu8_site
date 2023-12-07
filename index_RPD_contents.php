8bbe1fcb05f69e7a17f6b886ba9c0cf6a3952872
翔.jp <komiyama_a@hotmail.com>
2023-12-07 18:54:19 +0900

    烈風伝mod用のサイト・プレースホルダー

---
+++ index_RPD_contents.php
@@ -0,0 +1,21 @@
+<?php
+$content_hash = [
+	'401' =>
+        [ 'html'=>'401.html', 'title'=>'ホーム' , 'dir'=>''],
+
+    'nobu_ssr_home' =>
+        [ 'html'=>'nobu_ssr_home.html', 'title'=>'トップページ' , 'dir'=>''],
+
+    'nobu_mod_img_kahou' =>
+        [ 'html'=>'nobu_mod_img_kahou.html', 'title'=>'家宝グラ' , 'dir'=>''],
+];
+
+
+
+$filetime_hash = [
+    'nobu_mod_img_kahou' =>
+        './cnt_kahou/ITEMCG.zip',
+    'nobu_data_bushoulist' =>
+        './cnt_bushou/武将一覧.zip',
+];
+?>
