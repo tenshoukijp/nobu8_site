0d50a340eee9f5465b4db80464bd074af8b1255b
翔.jp <komiyama_a@hotmail.com>
2024-06-30 09:59:11 +0900

    こみっと

--- index_RPD.php
+++ index_RPD.php
@@ -4,13 +4,13 @@ require( "contents.php" );
 
 $punnyAddress = "xn--9oqr43f8k1a.jp-mod.net";
 
-$defaultHomePage = "nobu_rpd_home";
-
-$indexFileName = "index.html";
-
 $strTitle = "烈風伝.jp";
 
 $strDescription = "烈風伝の改造";
+
+$defaultHomePage = "nobu_rpd_home";
+
+$indexFileName = "index.html";
 
 require( "common.php");
 ?>
