f6c3f1072c63936c2817adb21b39914361805600
翔.jp <komiyama_a@hotmail.com>
2024-08-28 20:05:35 +0900

    こみっと

--- index_RPD.php
+++ index_RPD.php
@@ -6,7 +6,7 @@ $punnyAddress = "xn--9oqr43f8k1a.jp-mod.net";
 
 $strTitle = "烈風伝.jp";
 
-$strDescription = "烈風伝の改造";
+$strDescription = "「烈風伝」の「改造」を中心としたサイト";
 
 $defaultHomePage = "nobu_rpd_home";
 
